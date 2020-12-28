aeaf84f8b922b7bed9045b6be59785f1a36b98ff
Vergil <vpcola@gmail.com>
2020-12-28 15:24:54 +0800

    Added fix to remote

--- dustsensor_parser.c
+++ dustsensor_parser.c
@@ -174,7 +174,7 @@ dustsensor_parser_handle_t dustsensor_parser_init(const dustsensor_parser_config
     }
 
     /* Set pattern interrupt, used to detect the end of a line */
-    uart_enable_pattern_det_baud_intr(esp_dustsensor->uart_port, 'B', 1, 10000, 10, 10);
+    uart_enable_pattern_det_baud_intr(esp_dustsensor->uart_port, 'B', 1, 9, 0, 0);
     /* Set pattern queue size */
     uart_pattern_queue_reset(esp_dustsensor->uart_port, config->uart.event_queue_size);
     uart_flush(esp_dustsensor->uart_port);
